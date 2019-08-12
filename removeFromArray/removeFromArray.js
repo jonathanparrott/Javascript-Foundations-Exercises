01d39e441207c15286d015d042b4976d1d15ded9
Jonathan <parrottjonathanc@gmail.com>
2019-08-12 13:36:57 -0700

    Exercises updated with answers

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, badArg) {
+  let newArray = [];
+  let i;
+  for i in array {
+    if (i !== badArg) {
+      newArray += i;
+    }
+  return newArray
+  }
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+  if (times < 0) {
+    return "ERROR"
+  }
+  let newString = "";
+  for (let i = 0; i < times; i++) {
+    newString += word;
+  }
+  return newString
 }
 
 module.exports = repeatString
