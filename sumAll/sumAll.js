cb2c7e14be207d74b8f24bd29689e292b9d661c4
Jonathan <parrottjonathanc@gmail.com>
2019-08-22 20:51:19 -0700

    Finished all exercises to work with all test cases

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+  let sum = 0;
+  if ((typeof firstNum != "number") || (typeof secondNum != "number")) {
+    return "ERROR"
+  }
+    else if ((firstNum < 0) || (secondNum < 0)) {
+      return "ERROR"
+    }
 
-}
+    else if (firstNum > secondNum) {
+    
+      for (let i = secondNum; i < firstNum + 1; i++) {
+      sum += i;
+      }
+  }
+    else if (firstNum == secondNum) {
+      sum = 0;
+      }
 
-module.exports = sumAll
+    else {
+      for (let i = firstNum; i < secondNum + 1; i++) {
+        sum += i;
+    }
+  }
+
+  return sum
+    }
+
+module.exports = sumAll;
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let convertedC = (temp - 32) * (5 / 9);
+  if (Number.isInteger(convertedC)) {
+    return Number(convertedC)
+  }
+  
+  else {
+    return Number(convertedC.toFixed(1))
+  }
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let convertedF = temp * (9 / 5) + 32;
+  
+  if (Number.isInteger(convertedF)) {
+    return Number(convertedF)
+  }
+  else {
+    return Number(convertedF.toFixed(1))
+  }
 }
 
 module.exports = {
